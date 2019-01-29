2274497d283a675e2eec3e8b5f50a28376448467
Ghost141 <kidd747@gmail.com>
2019-01-29 13:30:56 +0800

    Add Min/Max method for int32&int64

--- math.go
+++ math.go
@@ -1,1 +1,29 @@
 package go_
+
+func MinInt64(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func MinInt32(a, b int32) int32 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func MaxInt64(a, b int64) int64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func MaxInt32(a, b int32) int32 {
+	if a > b {
+		return a
+	}
+	return b
+}
