f4baa0642bdbba14fda85523b7682a31a287f417
Ghost141 <kidd747@gmail.com>
2019-02-14 01:40:13 +0800

    Add more method in HashSet

--- set.go
+++ set.go
@@ -17,3 +17,29 @@ func (set HashSet) LoadWithDefault(key interface{}, defVal bool) bool {
 func (set HashSet) Remove(key interface{}) {
 	delete(set, key)
 }
+
+func (set HashSet) Keys() []interface{} {
+	res := make([]interface{}, 0, len(set))
+	for k := range set {
+		res = append(res, k)
+	}
+	return res
+}
+
+func (set HashSet) Size() int {
+	return len(set)
+}
+
+func (set HashSet) ForEach(f func(interface{})) {
+	for k := range set {
+		f(k)
+	}
+}
+
+func (set HashSet) Map(f func(interface{}) interface{}) []interface{} {
+	res := make([]interface{}, 0, len(set))
+	for k := range set {
+		res = append(res, f(k))
+	}
+	return res
+}
